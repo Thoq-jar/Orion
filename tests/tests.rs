23806189ac17bc7cb0ab3f2d138777142679a23b
Thoq-jar <tristanshaw000@gmail.com>
2024-07-23 17:48:55 -0400

    fixing git pt 1

---
+++ tests/tests.rs
@@ -0,0 +1,17 @@
+use std::process::exit;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        main();
+    }
+}
+
+fn main() {
+    // IntelliJ Wouldn't shut the fuck up without fn main()
+    println!("TODO: Write tests pt.2");
+    exit(0);
+}
